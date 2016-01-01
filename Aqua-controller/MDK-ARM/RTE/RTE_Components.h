51b8c20b399c309889baf9ce78fcd0ad65f75ae0
patlas <plas@student.agh.edu.pl>
2016-01-02 00:31:49 +0100

    Initial data commit

---
+++ Aqua-controller/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Aqua-controller' 
+ * Target:  'Aqua-controller Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ adb.bat
@@ -0,0 +1,3 @@
+C:
+cd C:\Users\PatLas\AppData\Local\Android\sdk\platform-tools
+adb.exe connect 127.0.0.1
